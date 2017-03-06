cc2ab4a6bfddae9896ce1f65a00dc68a85a9c3de
periofljj <peri.jing.l@gmail.com>
2017-03-06 15:28:19 +0800

    Linkbox Pages.

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,6 +26,26 @@
 		mobile: '(max-width: 736px)'
 	});
 
+	//goTotop
+		$(window).scroll(function() {		
+
+		if($(window).scrollTop() >= 100){
+
+			$('.actGotop').fadeIn(300); 
+
+		}else{    
+
+			$('.actGotop').fadeOut(300);    
+
+		}  
+
+	});
+
+	$('.actGotop').click(function(){
+
+	$('html,body').animate({scrollTop: '0px'}, 800);});	
+	//
+
 	$(function() {
 
 		var	$window = $(window),
